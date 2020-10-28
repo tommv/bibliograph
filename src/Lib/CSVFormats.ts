6ce4d33e291dd6dacedbb9423be9693423a48c9e
Alexis Jacomy <alexis.jacomy@gmail.com>
2020-10-28 16:29:07 +0100

    App template with create-react-app

---
+++ src/Lib/CSVFormats.ts
@@ -0,0 +1,66 @@
+import { CSVFormat } from "../../types";
+import { zip } from "lodash";
+
+export const CSVFormats: { [key: string]: CSVFormat } = {
+  webOfScience: {
+    label: "Web of Science",
+    url: "",
+    separator: "\t",
+    mandatoryFields: [
+      {
+        key: "CR",
+        variableName: "references",
+        separator: ";",
+      },
+      {
+        key: "PY",
+        variableName: "year",
+      },
+    ],
+    metadataFields: [
+      {
+        key: "AU",
+        variableName: "authors",
+        separator: ";",
+      },
+    ],
+  },
+  scopus: {
+    label: "Scopus",
+    url: "",
+    separator: ",",
+    mandatoryFields: [
+      {
+        key: "References",
+        variableName: "references",
+        separator: ";",
+      },
+      {
+        key: "Year",
+        variableName: "year",
+      },
+    ],
+    metadataFields: [
+      {
+        key: "Authors",
+        variableName: "authors",
+        separator: ",",
+      },
+      {
+        key: "Author(s) ID",
+        variableName: "authorsID",
+        separator: ";",
+      },
+    ],
+    generatedFields: [
+      {
+        maker: (line) =>
+          zip(
+            line.authors,
+            line.authorsID.filter((id) => id !== "")
+          ).map(([name, id]: string[]) => ({ name, id })),
+        variableName: "authors",
+      },
+    ],
+  },
+};
