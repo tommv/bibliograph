bdff40c3c633c4c72d0a4cef0eea75fdeec5117f
Paul Girard <paul@ouestware.com>
2020-10-29 10:49:45 +0100

    working on fulll graph generation

--- src/Lib/loadFullGraph.ts
+++ src/Lib/loadFullGraph.ts
@@ -1,4 +1,24 @@
-import Graph from "graphology";
+import Graph, { UndirectedGraph } from "graphology";
+import Papa from "papaparse";
+
+const csvToGraph = (pathsToParse:string[],graph:UndirectedGraph) => {
+  if (pathsToParse.length === 0)
+    return graph;
+  else {
+    Papa.parse(pathsToParse[0], {
+      worker: true,
+      step: function(row) {
+        // transform row into graph nodes and edges
+        console.log("Row:", row.data);        
+      },
+      complete: function() {
+        console.log("All done!");
+        return csvToGraph(pathsToParse.slice(1), graph)
+      }
+    });
+}
+
+}
 
 import { CSVFormat } from "./types";
 
@@ -6,5 +26,9 @@ export function loadFullGraph(
   paths: string[],
   format: CSVFormat
 ): Promise<Graph> {
-  return Promise.resolve(new Graph());
+  // will return a promise
+  const fullGraph = new UndirectedGraph();
+  csvToGraph(paths, fullGraph);
+
+  return Promise.resolve(fullGraph);
 }
