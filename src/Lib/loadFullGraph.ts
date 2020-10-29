462667eb1dafcb5f0a6437be9f82c536133d3c39
Paul Girard <paul@ouestware.com>
2020-10-29 13:15:48 +0100

    first draft for fullGraph

--- src/Lib/loadFullGraph.ts
+++ src/Lib/loadFullGraph.ts
@@ -1,32 +1,85 @@
-import Graph, { UndirectedGraph } from "graphology";
-import Papa from "papaparse";
+import { UndirectedGraph } from "graphology";
+import papa, { ParseResult } from "papaparse";
+import { Field, GeneratedField, CSVFormat } from "./types";
 
-const csvToGraph = (pathsToParse: string[], graph: UndirectedGraph) => {
-  if (pathsToParse.length === 0) return graph;
-  else {
-    Papa.parse(pathsToParse[0], {
-      worker: true,
-      step: function (row) {
-        // transform row into graph nodes and edges
-        console.log("Row:", row.data);
-      },
-      complete: function () {
-        console.log("All done!");
-        return csvToGraph(pathsToParse.slice(1), graph);
-      },
+const csvRowToGraph = (
+  csvRow: { [key: string]: string },
+  format: CSVFormat,
+  graph: UndirectedGraph
+) => {
+  console.log(csvRow);
+  // references
+  const refsField = format.references;
+  if (refsField && csvRow[refsField.key]) {
+    const references: string[] = (csvRow[refsField.key].split(
+      refsField.separator || ","
+    ) as string[]).map((ref) => {
+      return graph.addNode(ref, { label: ref, type: "reference" });
     });
-  }
-};
+    // metadata factory
+    const metadataNodes: string[] = [];
+    const metadata = format.metadataFields.reduce((meta: {}, f: Field) => {
+      // get value
+      let values = [];
+      // parse multiple values
+      if (f.separator) values = csvRow[f.key].split(f.separator);
+      else values.push(csvRow[f.key]);
+      // generate node if not hidden field
+      if (!f.hidden)
+        values.forEach((value: string) =>
+          metadataNodes.push(
+            graph.addNode(value, { label: value, type: f.variableName })
+          )
+        );
+      // craft a parsed line for generated fields
+      return { ...meta, [f.variableName]: values };
+    }, {});
+    // generated fields
+    if (format.generatedFields)
+      format.generatedFields?.forEach((f: GeneratedField) => {
+        const node = f.maker(metadata);
+        metadataNodes.push(
+          graph.addNode(node.key, { ...node, type: f.variableName })
+        );
+      });
 
-import { CSVFormat } from "./types";
+    // add edges between refs and metadata
+    references.forEach((ref) =>
+      metadataNodes.forEach((m) => {
+        const e = graph.addEdge(ref, m);
+        graph.mergeEdgeAttributes(e, {
+          weight: (graph.getEdgeAttribute(e, "weight") || 0) + 1,
+        });
+      })
+    );
+
+    return graph;
+  } else throw "references field is mandatory";
+  // TODO: add index on not nodes
+};
 
 export function loadFullGraph(
   paths: string[],
   format: CSVFormat
-): Promise<Graph> {
-  // will return a promise
+): Promise<UndirectedGraph> {
   const fullGraph = new UndirectedGraph();
-  csvToGraph(paths, fullGraph);
-
-  return Promise.resolve(fullGraph);
+  return Promise.all(
+    paths.map(
+      (path: string) =>
+        new Promise((resolve) => {
+          papa.parse(path, {
+            // add separator
+            delimiter: format.separator,
+            header: true,
+            step: function (row: ParseResult<{ [key: string]: string }>) {
+              // transform row into graph nodes and edges
+              csvRowToGraph(row.data[0], format, fullGraph);
+            },
+            complete: () => {
+              resolve();
+            },
+          });
+        })
+    )
+  ).then(() => fullGraph);
 }
