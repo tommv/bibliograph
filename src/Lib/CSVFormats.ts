24e825f4be6c3dd473eb79dcf0b2be8e17976faa
Tommaso Venturini <tomm.venturini@gmail.com>
2023-10-07 11:05:35 +0200

    Update CSVFormats.ts

--- src/Lib/CSVFormats.ts
+++ src/Lib/CSVFormats.ts
@@ -163,7 +163,7 @@ export const CSVFormats: { [key: string]: CSVFormat } = {
     metadataFields: [
       {
         variableName: "_authors",
-        variableLabel: "Author full names",
+        variableLabel: "Author names",
         key: "Authors",
         separator: ";",
         hidden: true,
